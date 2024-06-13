7c6fc0021726603860edf25fe49449b080e73f78
Sturdy10 <stg100t@gmail.com>
2024-06-13 18:03:15 +0700

    not dev

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,52 +3,67 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 
+
+
+
 const Accent = () => {
     return (
         <div className="w-screen h-screen flex">
-            <div className="w-[70%] h-full bg-custom-gradient ">
-                <h3>body1</h3>
+            {/* <div className="w-[70%] h-full bg-custom-gradient "> */}
+            
+            <div
+        className="w-[70%] h-full relative"
+        style={{
+          backgroundImage: 'url(/images/background01.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'top',
+        }}
+      >
+        
+           {/* <div className="w-[70%] h-full relative overflow-hidden">
+              <video
+                    autoPlay
+                    loop
+                    muted
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                >
+                    <source src="/video/background.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video> */}
             </div>
-            <div className=" w-[35%] h-full bg-white rounded-l-[20px]  absolute right-0 shadow-2xl flex justify-center py-20 ">
+            
+            <div className=" w-[35%] h-full bg-white  absolute right-0 shadow-2xl flex justify-center py-20 ">
                 <Tabs defaultValue="signin" className="w-[400px] ">
-                    <div className="flex justify-center py-5">
-                        <TabsList >
-                            <TabsTrigger value="signin">Sign In</TabsTrigger>
-                            <TabsTrigger value="signup">Sign UP</TabsTrigger>
+                    <div className="py-5">
+                        <TabsList className="bg-[#dbd7db] " >
+                            <TabsTrigger  value="signin">Sign In</TabsTrigger>
+                            <TabsTrigger value="signup">Sign Up</TabsTrigger>
                         </TabsList>
                     </div>
-
-
                     <TabsContent value="signin">
                         <div className="flex flex-col justify-center space-y-8 ">
-                            
-                                <div className="space-y-3">
-                                    <Label htmlFor="email"><h3>Email</h3></Label>
-                                    <Input type="email"/>
-                                </div>
 
-                                 <div className="space-y-3">
-                                    <Label  htmlFor="Password"><h3>Password</h3></Label>
-                                    <Input type="Password"/>
-                                 </div>
-                         <Button><h1>Sin In</h1></Button>                
+                            <div className="space-y-3">
+                                <Label htmlFor="email"><h3>Email</h3></Label>
+                                <Input type="email" />
+                            </div>
+
+                            <div className="space-y-3">
+                                <Label htmlFor="Password"><h3>Password</h3></Label>
+                                <Input type="Password" />
+                            </div>
+                            <Button type="button" className=" p-2 rounded hover:bg-[#757575] font-bold"> Sign In </Button >
                         </div>
                     </TabsContent>
-
                     <TabsContent value="signup">
-                    <div className="flex flex-col justify-center space-y-8 ">
-                                <div className="space-y-3">
-                                    <Label htmlFor="email"><h3>Email</h3></Label>
-                                    <Input type="email" />
-                                </div>
-                                <div>
-                                     <Button><h1>Sin Up</h1></Button>
-                                </div>
+                        <div className="flex flex-col justify-center space-y-8 ">
+                            <div className="space-y-3">
+                                <Label htmlFor="email"><h3>Email</h3></Label>
+                                <Input type="email" />
+                            </div>
+                            <Button type="button" className=" p-2 rounded hover:bg-[#757575] font-bold">Sin Up</Button>
                         </div>
                     </TabsContent>
-
-
-
                 </Tabs>
             </div>
         </div>
