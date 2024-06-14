3f1428545c2620bbf0ed9464d9716c4b9df84fce
Sturdy10 <stg100t@gmail.com>
2024-06-15 02:27:30 +0700

    Account completed

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,14 +1,25 @@
+"use client"
+import { useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox";
 import Image from "next/image"
 import Logo from "@/../public/images/logo.png"
 
 
 
 
+
+
+
 const Accent = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    }
     return (
         <div className="w-screen h-screen flex">
             {/* <div className="w-[70%] h-full bg-custom-gradient "> */}
@@ -35,9 +46,11 @@ const Accent = () => {
                 </video> */}
             </div>
 
-            <div className=" w-[35%] h-full bg-white  absolute right-0 shadow-2xl flex justify-center py-20 ">
-                <Tabs defaultValue="signin" className="w-[400px] flex flex-col gap-32">
-                    <div className="  ">
+            <div className=" w-[34%] h-full bg-white  absolute right-0 shadow-2xl flex justify-center py-11">
+
+                <Tabs defaultValue="signin" className="w-[60%] flex flex-col gap-20">
+                    <img src="iogin.svg" alt="next" className=" h-24 mx-auto" />
+                    <div>
                         <TabsList className="bg-[#dbd7db] " >
                             <TabsTrigger value="signin">Sign In</TabsTrigger>
                             <TabsTrigger value="signup">Sign Up</TabsTrigger>
@@ -51,10 +64,22 @@ const Accent = () => {
                                 <Input type="email" />
                             </div>
 
+
                             <div className="space-y-3">
-                                <Label htmlFor="Password"><h3>Password</h3></Label>
-                                <Input type="Password" />
+                                <Label htmlFor="password"> <h3>Password</h3> </Label>
+                                <Input type={showPassword ? "text" : "password"} id="password" />
+                                <div className="flex items-center space-x-2">
+                                    <div className="flex-grow"></div>
+                                    <div className="flex items-center space-x-2">
+                                        <Checkbox id="showPassword" onCheckedChange={togglePasswordVisibility} />
+                                        <label htmlFor="showPassword" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                                            Show Password
+                                        </label>
+                                    </div>
+                                </div>
                             </div>
+
+
                             <Button type="button" className=" p-2 rounded hover:bg-[#757575] font-bold"> Sign In </Button >
                         </div>
                     </TabsContent>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
- return(
-  <h1>Hello</h1>
- );
+    return (
+        <div className="w-screen h-screen flex justify-center items-center ">
+            <div className="w-[700px] h-[30%] bg-green-100 flex flex-col gap-4 p-4">
+                <div className="flex gap-4">
+                    <div className="flex-1">
+                        <label htmlFor="name" className="mb-2"><h3>Name</h3></label>
+                        <Input id="name" type="text" />
+                    </div>
+                    <div className="flex-1">
+                        <label htmlFor="surname" className="mb-2"><h3>Surname</h3></label>
+                        <Input id="surname" type="text" />
+                    </div>
+                </div>
+
+                <div className="flex gap-4">
+                    <div className="flex-1">
+                        <label htmlFor="phone">
+                            <h3>Phone</h3>
+                        </label>
+                        <Input id="phone" type="tel" className="p-4" />
+                    </div>
+                </div>
+
+                <div className="flex justify-end">
+                    <div className="flex justify-end mt-10">
+                        <Button type="button" className=" p-4 rounded hover:bg-[#757575] font-bold">
+                            Submit
+                        </Button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
+
 export default Home;
+
+
+
+
+
+
+
